105a60b678fb9f9849bbe0e45e08c5dabeaad4ca
KevinSantizo <kevinsantizo660@gmail.com>
2020-04-20 19:39:20 -0600

    First commit, Square Header

---
+++ lib/src/widgets/square_header.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300.0,
+      color: Color(0xffff7477),
+    );
+  }
+}
